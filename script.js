13b8f34a4897fc8a448ae9fa733d02eeb21b9ad5
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-12-15 16:13:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make=make;
+	this.model=model
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel=function(){
+
+	return this.make+' '+this.model
+}
+
+function SportsCar(make, model, topSpeed) {
+	super(make, model)
+	this.topSpeed=topSpeed;
+}
+
+SportsCar.prototype.getTopSpeed=function(){
+	return this.topSpeed
+}
+
+SportsCar.prototype.__proto__ = Car.prototype
 
 // Do not change the code below
 window.Car = Car;
